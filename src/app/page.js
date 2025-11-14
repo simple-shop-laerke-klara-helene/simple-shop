4247d94b4641e09c989311f2bf6a3e3dafc458f8
lakr0003 <lakr0003@stud.kea.dk>
2025-11-14 09:46:27 +0100

    feat: remove unused <hero> tag and clean up main structure in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,14 @@ export default function Home({ searchParams }) {
       <header className="col-[full] grid grid-cols-subgrid">
         <Header />
       </header>
-<hero>
-
-</hero>
+      <main className="col-[full] grid grid-cols-subgrid">
+        <div className="col-[content]">
+          <h1 className="text-3xl font-bold underline">SimpleShop!</h1>
+          <button className="mt-4 rounded bg-blue px-4 py-2 text-white hover:bg-blue-600">
+            Se Produkter
+          </button>
+        </div>
+      </main>
     </>
   );
 }
