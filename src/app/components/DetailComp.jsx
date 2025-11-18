6da4b33c302cb7f187c3ab2b39a966e35543578a
lakr0003 <lakr0003@stud.kea.dk>
2025-11-18 11:28:27 +0100

    style: enhance empty cart message and improve layout in BasketComponent; update DetailComponent to include AddToCartButton

--- src/app/components/DetailComp.jsx
+++ src/app/components/DetailComp.jsx
@@ -1,5 +1,6 @@
 import DetailPicsComp from "./DetailPicsComp";
 import BasketComponent from "./BasketComponent";
+import AddToCartButton from "./AddProductButton";
 import { FaStar, FaRegStar } from "react-icons/fa";
 
 const DetailComponent = async ({ params }) => {
@@ -8,27 +9,30 @@ const DetailComponent = async ({ params }) => {
   const product = await response.json();
   return (
     <>
-      <a href="/products" className="hover:text-dark-text cursor-pointer">
-        {"< tilbage"}
+      <a href="/products" className="hover:text-dark-text cursor-pointer mb-4">
+        {"< Go back to products"}
       </a>
       <div className="grid grid-cols-[2fr_1fr] gap-4">
         <section className="grid grid-cols-[1fr_1fr] gap-4">
           <DetailPicsComp product={product} />
           <div className="mt-2">
-            <h1>{product.title}</h1>
+            <h1 className="mb-2">{product.title}</h1>
             <p>{product.description}</p>
+            <div className="mt-8">
+              <AddToCartButton product={product} />
+            </div>
           </div>
         </section>
         <div>
           <BasketComponent />
         </div>
       </div>
-      <div class="mt-16 flex items-center">
-        <div class="h-2 w-2 rounded-full bg-black"></div>
+      <div className="mt-16 flex items-center">
+        <div className="h-2 w-2 rounded-full bg-black"></div>
 
-        <div class="h-0.5 flex-1 bg-black"></div>
+        <div className="h-0.5 flex-1 bg-black"></div>
 
-        <div class="h-2 w-2 rounded-full bg-black"></div>
+        <div className="h-2 w-2 rounded-full bg-black"></div>
       </div>
       <section className="mt-16">
         <div className="flex items-center gap-2">
@@ -51,17 +55,11 @@ const DetailComponent = async ({ params }) => {
 const renderStars = (rating) => {
   return Array.from({ length: 5 }, (_, i) => {
     const filled = i < Math.round(rating);
-    return filled ? (
-      <FaStar key={i} className="text-yellow-400" size={40} />
-    ) : (
-      <FaRegStar key={i} className="text-yellow-400" size={40} />
-    );
+    return filled ? <FaStar key={i} className="text-yellow-400" size={40} /> : <FaRegStar key={i} className="text-yellow-400" size={40} />;
   });
 };
 const renderExactStars = (rating) => {
-  return Array.from({ length: rating }, (_, i) => (
-    <FaStar key={i} className="text-yellow-400" size={40} />
-  ));
+  return Array.from({ length: rating }, (_, i) => <FaStar key={i} className="text-yellow-400" size={40} />);
 };
 
 export default DetailComponent;
